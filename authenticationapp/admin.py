879fcc98aba7a5b79c35f3e631d913b9044afe62
mirerakepha <kephamirera16@gmail.com>
2025-08-26 16:28:14 +0300

    fied errors

--- authenticationapp/admin.py
+++ authenticationapp/admin.py
@@ -8,11 +8,11 @@ from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'bio', 'profile_pic')      # Columns in list view
-    search_fields = ('user__username', 'bio')          # Searchable fields
-    list_filter = ('user__is_staff', 'user__is_active')  # Side filters
+    list_display = ('id', 'user', 'bio', 'image')
+    search_fields = ('user__username', 'bio')
+    list_filter = ('user__is_staff', 'user__is_active')
 
-# Optional: Extend the default User admin to show Profile inline
+
 class ProfileInline(admin.StackedInline):
     model = Profile
     can_delete = False
@@ -21,6 +21,6 @@ class ProfileInline(admin.StackedInline):
 class UserAdmin(DefaultUserAdmin):
     inlines = (ProfileInline,)
 
-# Unregister and re-register User to attach ProfileInline
+
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
--- authenticationapp/models.py
+++ authenticationapp/models.py
@@ -10,3 +10,24 @@ class Profile(models.Model):
     def __str__(self):
         return f"{self.user.username} Profile"
 
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        img = Image.open(self.image.path)
+
+        width, height = img.size
+        min_dim = min(width, height)
+
+        left = (width - min_dim) / 2
+        top = (height - min_dim) / 2
+        right = (width + min_dim) / 2
+        bottom = (height + min_dim) / 2
+
+        img = img.crop((left, top, right, bottom))
+
+        img = img.resize((300, 300), Image.Resampling.LANCZOS)
+
+
+        img.save(self.image.path, optimize=True, quality=85)
+
